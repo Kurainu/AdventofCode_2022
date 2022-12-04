4dd890be588239b9526f3f82c507fc8a3eaea2a7
theshiro01 <theshiro01@gmail.com>
2022-12-04 14:00:04 +0100

    Day 4

---
+++ 4/part2.ps1
@@ -0,0 +1,49 @@
+$part1_example = Get-Content -Path .\input\part1.example
+$part1_input = Get-Content -Path .\input\part1.input
+
+function Solution_example {
+    $count = 0 
+    foreach ($line in $part1_example) {
+        $flag = $false
+
+        $left, $right = $line.Split(',')
+
+        $left = $left.Replace("-","..") | Invoke-Expression
+
+        $right = $right.Replace("-","..") | Invoke-Expression
+
+        if (($right | Where-Object {$left -contains $_}).Count -ge 1) { $flag = $true }
+
+        if (($left | Where-Object {$right -contains $_}).Count -ge 1) { $flag = $true }
+
+        if ($flag) {
+            $count++
+        }
+    }
+    $count
+}
+
+function Solution_input {
+    $count = 0 
+    foreach ($line in $part1_input) {
+        $flag = $false
+
+        $left, $right = $line.Split(',')
+
+        $left = $left.Replace("-","..") | Invoke-Expression
+
+        $right = $right.Replace("-","..") | Invoke-Expression
+
+        if (($right | Where-Object {$left -contains $_}).Count -ge 1) { $flag = $true }
+
+        if (($left | Where-Object {$right -contains $_}).Count -ge 1) { $flag = $true }
+
+        if ($flag) {
+            $count++
+        }
+    }
+    $count
+}
+
+Write-Host "Output Example Part1: $(Solution_example)"
+Write-Host "Output Input   Part1: $(Solution_input)"
